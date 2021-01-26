28ceb3db263c5c4f29add95a19dc4ee3ccbee504
alvin zhao <yibozhao2018@gmail.com>
2021-01-25 21:37:06 -0800

    add column to toys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_052934) do
+ActiveRecord::Schema.define(version: 2021_01_26_053255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_052934) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "dog_id", null: false
   end
 
 end
