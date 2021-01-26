f8083e077fd20a4433afebe9e109b57572d4c104
alvin zhao <yibozhao2018@gmail.com>
2021-01-25 21:32:01 -0800

    toys

--- db/migrate/20210126052934_create_toys.rb
+++ db/migrate/20210126052934_create_toys.rb
@@ -1,7 +1,7 @@
 class CreateToys < ActiveRecord::Migration[5.2]
   def change
     create_table :toys do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_052553) do
+ActiveRecord::Schema.define(version: 2021_01_26_052934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "dogs", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "toys", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
